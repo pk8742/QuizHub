f80cae5a82fe547450591e1f192518bc03fb3741
Pravesh Kumar Sharma <63771757+pk8742@users.noreply.github.com>
2020-08-09 10:05:36 +0530

    Update settings.py

--- QuizHub/settings.py
+++ QuizHub/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'QuizHub',
         'USER': 'postgres',
-        'PASSWORD': 'pk8742',
+        'PASSWORD': '******',
         'HOST': 'localhost',
         'PORT': 5432
     }
